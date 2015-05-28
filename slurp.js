735d29ab49f0a6640fad45184330f36fcb96d705
Kurt Kaufman <kurt.m.kaufman@gmail.com>
2015-05-27 20:52:25 -0500

    Close enough.
    Initial commit. Support for one slurp.

---
+++ slurp.js
@@ -0,0 +1,47 @@
+/*
+ * slurp.js
+ * Free to use under my own, personal DBAA (Don't be an asshole) license.
+ * Hint: that's the entire text of the license
+ * Talk to your doctor before using slurp.js.
+*/
+
+
+(function($) {
+
+	//if your browser doesn't support audio then you should get gud
+	
+    $.fn.slurp = function(sickOptionsBreh) {
+        return this.each(function() {
+			
+			/*if you want to specify slurp noise, just pass in the name of the sound you want*/
+			var casualModeSettings = {
+				slurpNoise: "casual_tier"
+			};
+			
+			var sickOptionsBreh = $.extend(casualModeSettings, sickOptionsBreh);
+			
+			var _this = $(this);
+			
+			/*satan loves html in javascript, so we'll do that too */
+			var slurpHTML = '<audio id="slurpIntensifies" preload="auto"><source src="slurps/' + sickOptionsBreh.slurpNoise 
+							+ '.wav" /><source src="slurps/' + sickOptionsBreh.slurpnoise + '.ogg" /></audio>';	
+					
+			$('body').append(slurpHTML); 
+			
+			function init() {
+			
+				//ayy lmao
+				function playSound() {
+					document.getElementById('slurpIntensifies').play();
+				}
+				playSound();
+				}
+			
+			_this.bind('click', function(e) {
+				init();
+			})
+			
+        });
+    }
+})(jQuery);
+
