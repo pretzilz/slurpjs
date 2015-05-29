a8e2146e497a6418774b0936ff44eb7426035bfa
Kurt Kaufman <kurt.m.kaufman@gmail.com>
2015-05-28 19:59:01 -0500

    Random support
    Added support for just choosing a random slurp.
    More comments.

--- slurp.js
+++ slurp.js
@@ -17,17 +17,32 @@
 				slurpNoise: "casual_tier"
 			};
 			
+		//this line does things
 		var sickOptionsBreh = $.extend(casualModeSettings, sickOptionsBreh);
-	
+		
+		//list of slurp names
+		var slurps = ["wat", "velociraptor", "sanic", "ohgod", "obnoxious", "mrboneswildride", "happy", "hailsatan", "casual_tier"];
+		
         return this.each(function() {
 			
-			/*if you want to specify slurp noise, just pass in the name of the sound you want*/
+			/*if you want to specify slurp noise, just pass in the name of the sound you want (or random)*/
 			
 			var _this = $(this);
 			
 			/*satan loves html in javascript, so we'll do that too */
-			var slurpHTML = '<audio id="slurpIntensifies" preload="auto"><source src="slurps/' + sickOptionsBreh.slurpNoise 
-							+ '.wav" /><source src="slurps/' + sickOptionsBreh.slurpNoise + '.ogg" /></audio>';	
+			if (sickOptionsBreh.slurpNoise == "random")
+			{
+				//if it's random, just load a default sound, it'll be changed (just trust me)
+				var slurpHTML = '<audio id="slurpIntensifies" preload="none"><source id = "wavSound" src="slurps/casual_tier.wav" /><source id = "oggSound" src="slurps/casual_tier.ogg" /></audio>';	
+			}
+			
+			else {
+				
+				//otherwise play your dumb choice
+				var slurpHTML = '<audio id="slurpIntensifies" preload="auto"><source id = "wavSound" src="slurps/' + sickOptionsBreh.slurpNoise 
+							+ '.wav" /><source id = "oggSound" src="slurps/' + sickOptionsBreh.slurpNoise + '.ogg" /></audio>';	
+			}
+			
 					
 			$('body').append(slurpHTML); 
 			
@@ -35,8 +50,23 @@
 			
 				//ayy lmao
 				function playSound() {
+					if (sickOptionsBreh.slurpNoise == "random")
+					{
+						//choose a slurp at random
+						var randomSlurp =  slurps[Math.floor(Math.random() * slurps.length)];	
+						
+						//change the sources
+						$("#wavSound").attr("src", "slurps/" + randomSlurp + ".wav");			
+						$("#oggSound").attr("src", "slurps/" + randomSlurp + ".ogg");
+						
+						//load that sh*t
+						document.getElementById('slurpIntensifies').load();						
+					}
+					
+					//play that sh*t
 					document.getElementById('slurpIntensifies').play();
 				}
+		
 				playSound();
 				
 				}
